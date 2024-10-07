059691d4e69e3732579b6809c1ecda852c4ff1ba
GoodBoySK <87279821+GoodBoySK@users.noreply.github.com>
2024-10-07 22:33:19 +0200

    added other basic webpages to routes

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,13 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from './views/Home.vue'
+import Explore from './views/Explore.vue';
+import Tips from './views/Tips.vue';
+import BlogPosts from './views/BlogPosts.vue';
 
 let router = createRouter({
     history: createWebHashHistory(),
     routes: [
         {path: '/', component: Home },
-        {path: '/explore', component: Home },
-        {path: '/tips', component: Home },
-        {path: '/blog', component: Home }
+        {path: '/explore', component: Explore },
+        {path: '/tips', component: Tips },
+        {path: '/blog', component: BlogPosts }
     ]
 
 });
