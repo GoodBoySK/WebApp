daccd304cbbf2daac4e92b51b885a7c511c26a8e
GoodBoySK <87279821+GoodBoySK@users.noreply.github.com>
2024-10-13 14:49:20 +0200

    Footer update

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,12 +7,31 @@ import { RouterView } from 'vue-router';
 <template>
     <navigation ></navigation>
     <router-view></router-view>
-    <footer class="blockquote-footer">
-      <h1>FOOTER</h1>
+    <footer class="blockquote-footer bg-primary  mb-0 mt-3 text-center text-white-50">
+      <div class="fs-6 footer-main d-flex justify-content-center py-3">
+        <a class="me-3" href="#">Ochrana osobných údajov</a>
+        <a class="" href="#">Kontakt</a>
+      </div>
+      <div class="d-flex footer-mentions justify-content-center py-3 bg-black bg-opacity-25 mt-3">
+          <p class="mb-0">&copy; kucharka.sk all rights reserverd</p>
+          <div class="vr mx-3"></div>
+          <p class="mb-0">Designed by <strong>Michal Šovčík</strong> </p>
+          <div class="vr mx-3"></div>
+          <p class="mb-0">Developed by <strong>Michal Šovčík</strong> </p>
+      </div>
     </footer>
 </template>
 
 <style lang="scss">
 @import 'assets/main.scss';
+  .footer-main a{
+    text-decoration: none;
+    color: $white;
+    opacity: 0.5;
+    &:hover {
+      opacity: 1;
+    }
+  }
+
 
 </style>
